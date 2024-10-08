306451e45b649b4f0a2981dfa13d29688545554c
Jonas Muehlethaler <github.com.driveway258@passmail.net>
2024-10-08 18:37:13 +0200

    V2

--- TopologyAPP.py
+++ TopologyAPP.py
@@ -1,28 +1,27 @@
 #platform.openai.com
 from openai import OpenAI
 import streamlit as st
-#from config import greeting
-#from gpt import get_openai_response
+from config import *
 
-client = OpenAI(api_key=open("./source/llm/keys.txt", "r").read().strip())
+client = OpenAI(api_key=open("./keys.txt", "r").read().strip())
 
 def chat_with_openai(input_text):
     chat_completion = client.chat.completions.create(
-        messages=input_text,
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            *input_text
+            ],
         max_tokens=400,  # Begrenze die Antwortlänge
         temperature=0.5,  # Bestimmt, wie kreativ die Antworten sind (niedriger = deterministischer)
-        model="gpt-4o",
     )
     return chat_completion.choices[0].message.content
 
 st.title("Your Topology Selector")
 if 'messages' not in st.session_state:
     st.session_state['messages'] = [
-        {"role": "system", "content": "You propose the most suitable power electronics toplogy based on the given requirements. If unclear, you ask for further inputs. Always make sure that voltage ranges (input and output), power level, isolation requirements are given. You can provide options of topologies with some background. Reply with a short explanation why this is the case. Never reply to a question that is not related to power electronics topologies."}
+        {"role": "system", "content": greeting}
     ]
-
-# User input
-st.text_area("","Hello, please provide me with details such as voltage ranges, power levels, isolation requirements, and EMI considerations. Feel free to add any additional information. I will then provide you with a topology proposal.\n")
 
 for message in st.session_state.messages:
     if message["role"] == "user":
@@ -46,31 +45,3 @@ if prompt := st.chat_input("..."):
 
     # Add assistant's message to session state
     st.session_state.messages.append({"role": "assistant", "content": response})
-
-
-# user_input = st.text_input("You:", key='input')
-
-# if st.button("Send"):
-#     if user_input:
-#         # User message
-#         st.session_state['messages'].append({"role": "user", "content": user_input})
-
-#         # Assistant's response
-#         with st.spinner("typing..."):
-#             try:
-#                 assistant_reply = chat_with_openai(st.session_state['messages'])
-#             except Exception as e:
-#                 st.error(f"An error occurred: {e}")
-#                 assistant_reply = "I'm sorry, but I'm unable to process your request at the moment."
-
-#         #print(assistant_reply)
-#         #Append assistant's reply
-#         #st.session_state['messages'].append({"role": "assistant", "content": assistant_reply})
-#         st.text_area("",assistant_reply, height=300)
-
-#         # # Clear input
-#         # st.session_state['input'] = ''
-
-#         # Refresh the app
-#         #st.experimental_rerun()
-
