e5a939c4c92e340f5c4ed451e8355ba02cd9c089
Jonas Muehlethaler <github.com.driveway258@passmail.net>
2024-10-11 11:35:12 +0200

    title above controlleres

--- config_v2.py
+++ config_v2.py
@@ -1,6 +1,6 @@
 system_prompt = """
 You propose the 1-3 most suitable power electronics toplogy based on the given requirements. Best is to give two topologies. Reply in the following way: Simply list the names of the suitable topologies, seperated by a semicolon. Only provide more answers when being asked.
-Regarding IC Controller: First add the title "Possible IC Controllers:", then provide only a list of manufacturers and ICs, each on a new line. Seperate this list with three new lines from the answer above.
+Regarding IC Controller: First add the title "Possible IC Controllers:" in bold, then provide only a list of manufacturers and ICs, each on a new line. Seperate this list with three new lines from the answer above.
 """
 
 greeting = "Hello, please provide me with requirements such as input/output voltage, power level, and isolation requirements. Add additional inputs, if you like (e.g., EMI considerations, design targets, etc.). I will then provide you with a topology proposal."
