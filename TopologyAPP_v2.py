1ac4d40850ad9686a23aad90af307822c38fdd93
Jonas Muehlethaler <github.com.driveway258@passmail.net>
2024-10-11 10:22:52 +0200

    new app version

---
+++ TopologyAPP_v2.py
@@ -0,0 +1,82 @@
+#platform.openai.com
+import streamlit as st
+from openai import OpenAI
+from config_v2 import *
+
+client = OpenAI(api_key=open("./source/llm/keys.txt", "r").read().strip())
+
+def chat_with_openai(input_text):
+    chat_completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            *input_text
+            ],
+        max_tokens=800,  # Begrenze die Antwortlänge
+        temperature=0.1,  # Bestimmt, wie kreativ die Antworten sind (niedriger = deterministischer)
+    )
+    return chat_completion.choices[0].message.content
+
+# Title for the app
+st.title("Topology Selector")
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": greeting}
+    ]
+
+# Text
+st.write(greeting)
+
+# Input fields
+vin = st.text_input("Input Voltage (Please do not forget to indicate whether it is AC or DC)", "")
+vout = st.text_input("Output Voltage (AC or DC?)", "")
+power = st.text_input("Power", "")
+galvanic_isolation = st.checkbox("Galvanically isolated?")
+anything = st.text_input("Anything else to be considered?", "")
+
+st.session_state.messages.append({"role": "user", "content": f"Vin: {vin}, Vout: {vout}, Power: {power}, Galvanically isolated: {'Yes' if galvanic_isolation else 'No'}, {anything}"})
+
+if 'button_labels' not in st.session_state:
+    st.session_state.button_labels = []
+
+# Button to start the analysis
+if st.button("Start Analysis"):
+    with st.spinner("Thinking..."):
+        response = chat_with_openai(st.session_state.messages)
+    st.session_state.button_labels = response.split(";")
+    j = len(st.session_state.button_labels)
+    print(st.session_state.button_labels)
+
+if st.session_state.button_labels:
+    st.write("The topology/topologies that suite your requirements best is/are:")
+    for label in st.session_state.button_labels[:-1]:
+        if st.button(label):
+            st.session_state.messages.append({"role": "user", "content": f"Why you chose {label} and what controller IC would suite best for above topologie(s)? (keep the answer short and put a distance between the two answers)"})
+            with st.spinner("Thinking..."):
+                response = chat_with_openai(st.session_state.messages)
+            st.write("[Click here for a detailed topology analysis](https://frenetic.ai/)")
+            st.text_area(response)
+            
+
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+
+# # Button for more details
+# if st.session_state.button_labels:
+#     if st.button("Explanation of choice"):
+#         with st.spinner("Thinking..."):
+#             st.write(st.session_state.button_labels[-1])
+
+# # Suggest IC controllers button and text field
+# # Button for more details
+# if st.session_state.button_labels:
+#     if st.button("Suggest IC controllers"):
+#         st.session_state.messages.append({"role": "user", "content": "What Controller IC would suite best for above topologie(s)?"})
+#         with st.spinner("Thinking..."):
+#             response = chat_with_openai(st.session_state.messages)
+#         st.write("Based on your inputs, the following IC controllers are suggested:")
+#         st.text_area(response)
+
--- config.py
+++
@@ -1,8 +0,0 @@
-system_prompt = """
-You propose the most suitable power electronics toplogy based on the given requirements. If unclear, you ask for further inputs. Always make sure that voltage ranges (input and output), 
-power level, isolation requirements are given. You can provide options of topologies with some background. Reply with a short explanation why this is the case. Never reply to a question that 
-is not related to power electronics topologies. When there is a clear topology selected, ask the user whether he wants controller ICs for this particular topology as well as suggest to go to 
-frenetic.ai to put in details of the topology to get waveforms and design magnetic components. Please put this question in bold.
-"""
-
-greeting = "Hello, please provide me with details such as voltage ranges, power levels, isolation requirements, and EMI considerations. Feel free to add any additional information. I will then provide you with a topology proposal."
