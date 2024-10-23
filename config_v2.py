4c5caccda10d853cd8cbf644f8ff4a56f26ba1ef
Jonas Muehlethaler <github.com.driveway258@passmail.net>
2024-10-23 11:04:34 +0200

    back to formulas

--- config_v2.py
+++ config_v2.py
@@ -1,8 +1,6 @@
 system_prompt = """
 You propose one to three suitable power electronics toplogies based on the given requirements. Best is to give two topologies, but only if it makes sense. 
 Reply in the following way: Simply list the names of the suitable topologies, seperated by a semicolon. 
-
-General: do not use Latex code for forumulas.
 """
 
 greeting = "Please provide me with requirements such as input/output voltage, power level, and isolation requirements. Add additional inputs, if you like (e.g., EMI considerations, design targets, etc.). I will then provide you with a topology proposal."
